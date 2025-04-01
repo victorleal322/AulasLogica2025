90c7426dae9327c087dc9e256f231022dc2e29cf
Victoe Leal <vitinho.leal.gta@gmail.com>
2025-04-01 15:15:29 -0300

    aprendendo git

---
+++ aula12.js
@@ -0,0 +1,33 @@
+//variaveis compostas / vetores / arrays
+
+//variaveis sumples, cabe apensa 1 dado por vez
+let fruta = 'maça'
+fruta = 'banana'
+
+let frutas = ['maça', 'banana', 'abacaxi', 'uva']
+
+console.log(frutas)
+console.log(frutas[1])
+
+//declarando lista vazia
+let listavazia = []
+
+//declaração de uma lista de numeros
+let numeros = [1, 2, 8, 5, 9]
+
+//desclarando uma lista de string
+let nomes = ['ana', 'jao','maria']
+
+//declarando uma lista de mista
+let listamista = [ 4, 'feijao', true, 7.55]
+
+// declarando uma lista com outras listas
+let listadevetores = [['coca cola', 'hot dog'], [5.00, 10.00]]
+console.log(listadevetores[0][1])
+
+
+ frutas = ['maça', 'banana', 'morango', 'abacaxi', ' melancia', 'laranja', 'uva']
+ console.log(frutas[4])
+ console.log(frutas.slice(0,4));
+ console.log(frutas.slice[1])
+ 
